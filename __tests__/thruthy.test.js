8406147b42c322982118bb9404bae7a02c61615b
Sachin Kumar <schnkumar762@gmail.com>
2026-09-10 13:46:10 +0530

    feat: Add thruthy operators test case

---
+++ __tests__/thruthy.test.js
@@ -0,0 +1,21 @@
+test("thruthy operators", () => {
+  var name = "algocamp";
+  var n = null;
+  expect(n).toBeNull();
+  expect(name).not.toBeNull();
+
+  expect(name).toBeTruthy();
+  expect(n).toBeFalsy();
+
+  var num1 = 100;
+  var num2 = -20;
+
+  expect(num1).toBeGreaterThan(10);
+});
+
+/*
+
+tobe
+tonotbe
+
+*/
